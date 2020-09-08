a7e07e1201b35662db6485c04c90e48a6581bb1d
Tian Yang <tianyan@cisco.com>
2020-09-08 15:03:22 +0800

    tuesday commit

--- Wordcounts/main.py
+++ Wordcounts/main.py
@@ -1,21 +1,21 @@
-# def countsamewords(words):
-#     sample = {}
-#     for word in words:
-#         if word in sample:
-#             sample[word] = sample[word] + 1
-#         else:
-#             sample[word] = 1
-#     return sample
-
-
-def countsamewords(words, setwords):
+def countsamewords(words):
     sample = {}
-    for word in setwords:
-        sample[word] = 0
     for word in words:
-        sample[word] = sample[word] + 1
-
+        if word in sample:
+            sample[word] = sample[word] + 1
+        else:
+            sample[word] = 1
     return sample
+
+
+# def countsamewords(words, setwords):
+#     sample = {}
+#     for word in setwords:
+#         sample[word] = 0
+#     for word in words:
+#         sample[word] = sample[word] + 1
+#
+#     return sample
 
 
 words = []
@@ -39,7 +39,7 @@ with open("I have a dream.txt","r") as f:
     set_words = set(words)
     print(len(words))
     print(len(set_words))
-    # temp = countsamewords(words)
-    temp = countsamewords(words, set_words)
+    temp = countsamewords(words)
+    # temp = countsamewords(words, set_words)
     print(temp)
 
---
+++ temp/main.py
@@ -0,0 +1,29 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+class IntList:
+
+    def __init__(self, f, r):
+        self.first = f
+        self.rest = r
+
+    def recursivesize(self):
+        if self.rest is None:
+            return 1
+        else:
+            return 1 + self.rest.size()
+
+    def notrecursive(self):
+        pointer = self
+        total_size = 0
+        while pointer is not None:
+            total_size += 1
+            pointer = pointer.rest
+        return total_size
+
+l = IntList(5, None)
+l = IntList(10, l)
+l = IntList(15, l)
+
+print(l.size())
