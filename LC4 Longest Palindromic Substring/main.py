c7dd22d0831fc7931c6b34f2c2c87f33c3b60eb8
UAECETian <magical2by@gmail.com>
2020-10-15 11:19:20 +0800

    after holiday

--- LC4 Longest Palindromic Substring/main.py
+++ LC4 Longest Palindromic Substring/main.py
@@ -7,10 +7,22 @@ class Solution:
         left = 0
         right = 0
 
+        for i in xrange(len(s)):
+            res = self.getthestr(s, i, i)
+            if len(res) > len(temp):
+                temp = res
+            res = self.getthestr(s, i, i+1)
+            if len(res) > len(temp):
+                temp = res
+        return temp
 
 
 
-        while(left >= 0 and right < len(s) and s[left] == s[right]):
-            left -= left
-            right += right
+    def getthestr(self,s,l,r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l = l - 1
+            r = r + 1
+        return s[l+1:r]
 
+tmp = Solution()
+print(tmp.longestPalindrome('aabaa'))
